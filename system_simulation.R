fe8456a8a5e06d03c94a46c4da94da1af2fccca1
jonchen1991 <jonchen1991@gmail.com>
2019-12-15 23:44:33 -0500

    merge to master after adding flowrate portion. Need to figure out how flowrates fluctuate and how to normalize.

--- system_simulation.R
+++ system_simulation.R
@@ -7,7 +7,7 @@ pop_size = 1e3
 # Helper functions
 next_dna <- function(dna_end){
   return(switch(dna_end, 
-                # a: NIA, b: R Delinquency, c: TDI, d: Correspondence, e: R Secured
+                # a: NIA, b: R Delinquency, c: TDI, d: Correspondence, e: R Secured, f: collected
                 'a'= 'b',
                 'b'= sample(c('c','d','e'), 1,prob = c(1,1,2)),
                 'c'= sample(c('d','e'), 1, prob = c(1,2)),
@@ -30,13 +30,27 @@ end_time <- Sys.time()
 print(end_time - start_time)
 
 # Step 2: From DNA strands, determine flow rates between nodes
-test<-as.data.frame(matrix(0,nrow=6, ncol=6, dimnames = list(c('a','b','c','d','e','f'),c('a','b','c','d','e', 'f'))))
-# source is row index, destination is column index
+
+nodes = c('a','b','c','d','e','f')
+
+# create flowrate matrix: source is row index, destination is column index
+flowcounts <- as.data.frame(matrix(0,nrow=6, ncol=6, dimnames = list(nodes, nodes)))
+
+# count how cases flow from one node to another from DNA strands
 for (i in 1: pop_size){
-  for (j in 1: length(dna_list[[i]]-1)){
-    
-    
+  for (j in 1: length(dna_list[[i]])-1){
+    flowcounts[dna_list[[i]][j],dna_list[[i]][j+1]] <- flowcounts[dna_list[[i]][j],dna_list[[i]][j+1]] + 1
   }
 }
+
+# translate raw counts into flow rates. Each entry is divided by the total cases in the row.
+flowrates <- flowcounts
+for (i in nodes){
+  flowrates[i,] <- flowrates[i,]/sum(flowrates[i,])
+}
+
+#drop a as a destination, f as a source
+#flowrates['f',] <- NULL
+#flowrates[,'a'] <- NULL
 # Step 3: Simulate how cases flow between nodes and determine where bottlenecks are.
 
